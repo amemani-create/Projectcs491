23b8ad516e5adda34451974f4f696f135f543e2c
amemani-create <78717013+amemani-create@users.noreply.github.com>
2021-06-29 11:47:32 -0400

    Postgresql Heroku setup

--- Projectcs491/settings.py
+++ Projectcs491/settings.py
@@ -81,13 +81,26 @@ WSGI_APPLICATION = 'Projectcs491.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Projectcs491',
+        'USER': 'postgres', 
+        'PASSWORD': 'memani00',
+        'HOST': 'localhost',
+        'PORT': '5433',
+        
+    }
+}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
